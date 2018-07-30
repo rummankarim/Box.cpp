1b2880fdef876b382c66c0b00b43386a5b211a64
Rumman Karim <rumman.23197@gmail.com>
2018-07-29 21:48:40 -0400

    Add files via upload

---
+++ box1.cpp
@@ -0,0 +1,49 @@
+/*
+ Rumman-Al-Karim
+ CSI 136
+ Lab 4A
+ This programm asks the uyser for a width and height and prints out a box
+ */
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main () {
+    // variables
+    int width, height;
+    
+    // user input
+    cout <<"Enter a width: " << endl;
+    cin >> width;
+    
+    cout << "Input height: " << endl;
+    cin >> height;
+    
+    if (width > 100 || height > 100) {
+        cout <<" Please renter valid inputs" << endl;
+    }
+    else {
+        
+    
+    
+    for (int row=0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+            
+            cout << "*";
+            
+        } // end of inner loop
+        
+        cout << endl;
+    } // end of outer loop
+    
+    
+    }
+    
+    
+    
+    
+    return 0;
+    
+}
